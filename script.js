beb05597b592b5bf3e76a082d48876c92fda97b1
HeinWin18 <justinmyowin222@gmail.com>
2022-12-29 16:15:50 -0800

    Added_Java

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const date = new Date();
 
+date.setDate(1); 
+
 const monthDays = document.querySelector(".days");
 
-const month = date.getMonth();
+const lastDay = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDate();
 
-console.log(month);
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(),0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDay();
+
+const nextDays = 7 - lastDayIndex + 6;
 
 const months = [
     "January",
@@ -27,8 +35,20 @@ document.querySelector(".date p").innerHTML = date.toDateString();
 
 let days = ""; 
 
-for(let i = 1; i <= 31; i++){
-    days += `<div>${i}</div>`;
+for(let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+}
+
+for(let i = 1; i <= lastDay; i++){
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+    days += `<div class = "today">${i}</div>`;
+    }else{
+        days += `<div>${i}</div>`;
+    }
+}
+
+for(let j = 1; j <= nextDays; j++){
+    days += `<div class="next-date">${j}</div>`
     monthDays.innerHTML = days;
 }
  
