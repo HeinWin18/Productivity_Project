6b65d295d9512c71e42c93e245440933b9b6a349
HeinWin18 <justinmyowin222@gmail.com>
2023-01-17 09:13:56 -0800

    Commit Event-Modal

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+//User clicks monthDays --> show a pop up --> User can enter event
+//User double clicks monthDays --> show a side panel (With the events)
+
 const date = new Date();
 
 const renderCalendar = () => {
     date.setDate(1); 
 
 const monthDays = document.querySelector(".days");
-
 const lastDay = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDate();
-
 const prevLastDay = new Date(date.getFullYear(), date.getMonth(),0).getDate();
-
 const firstDayIndex = date.getDay();
-
 const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDay();
-
 var nextDays = 7 - lastDayIndex - 1;
+
+const openModel = document.querySelector('.days'); //dot to select all all the days
+const modalcontainer = document.getElementById('modalcontainer');
+const eventTitleInput = document.getElementById('eventTitleInput');
+const saveButton = document.getElementById('saveButton');
+const cancelButton = document.getElementById("cancelButton");
+
+window.onload=function(){
+openModel.addEventListener('click', ()=>{
+    modalcontainer.classList.add('show');
+});
+
+cancelButton.addEventListener('click', ()=>{
+    modalcontainer.classList.remove('show');
+});
+}
 
 const months = [
     "January",
@@ -35,7 +49,6 @@ document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 document.querySelector(".date p").innerHTML = date.toDateString();
 
 let days = ""; 
-
 var addeddays = 0;
 
 for(let x = firstDayIndex; x > 0; x--, addeddays++) {
@@ -51,7 +64,7 @@ for(let i = 1; i <= lastDay; i++, addeddays++){
 }
 
 if(addeddays <= 35)
-    nextDays += 7;
+nextDays += 7;
 
 for(let j = 1; j <= nextDays; j++)
     days += `<div class="next-date">${j}</div>`
