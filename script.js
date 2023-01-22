4b8fad9b76233ecebe83021041b46d1e5908566f
HeinWin18 <justinmyowin222@gmail.com>
2023-01-22 08:50:49 -0800

    Commit_event

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-//User clicks monthDays --> show a pop up --> User can enter event
-//User double clicks monthDays --> show a side panel (With the events)
+//Delete events from the side panel
+//Fix bugs while adding deleteEvent
+//Multiple events
+//Reset Header 2 --> Show the right date
 
 const date = new Date();
+let selected_date = null;
 
 const renderCalendar = () => {
     date.setDate(1); 
 
+//Calender
 const monthDays = document.querySelector(".days");
 const lastDay = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDate();
 const prevLastDay = new Date(date.getFullYear(), date.getMonth(),0).getDate();
@@ -13,21 +17,76 @@ const firstDayIndex = date.getDay();
 const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+ 1,0).getDay();
 var nextDays = 7 - lastDayIndex - 1;
 
-const openModel = document.querySelector('.days'); //dot to select all all the days
+//Pop-Up
+const openModel = document.querySelector('.days'); //dot to select all the days
 const modalcontainer = document.getElementById('modalcontainer');
 const eventTitleInput = document.getElementById('eventTitleInput');
 const saveButton = document.getElementById('saveButton');
-const cancelButton = document.getElementById("cancelButton");
+const cancelButton = document.getElementById('cancelButton');
 
-window.onload=function(){
-openModel.addEventListener('click', ()=>{
-    modalcontainer.classList.add('show');
+//Side-panel
+const sidepanelcontainer = document.getElementById('sidepanelcontainer');
+const eventText = document.getElementById('eventText');
+const closeButton = document.getElementById('closeButton');
+var clickCount = 0;
+
+//Single & Double click
+openModel.addEventListener('click', function(event){
+    clickCount++;
+    selected_date = event.target;
+    if(clickCount == 1){
+        singleClickTimer = setTimeout(()=> {
+        clickCount = 0;
+        modalcontainer.classList.add('show');
+    }, 160);
+    }else if(clickCount == 2 && clickCount != 1){
+        clearTimeout(singleClickTimer);
+        clickCount = 0;
+        sidepanelcontainer.classList.add('show');
+    }
+});
+//2D array
+let calendarEvents = new Array(12);
+for (let i = 0; i < calendarEvents.length; i++){
+    calendarEvents[i] = new Array(31);
+}
+
+function saveEvent(){
+    // const addEvent = (calendarEvents) => {
+    // let calendarEvent = {
+    //         title : eventTitleInput.value
+    //    } 
+    //    addEvent.push(calendarEvents);
+    //    localStorage.setItem('eventList', JSON.stringify(calendarEvents));
+    // }
+    let month = date.getMonth();
+    let day = parseInt(selected_date.innerText);
+    let calenderEvent = {title: eventTitleInput.value};
+   
+    if(calenderEvent){
+        eventText.innerText = calenderEvent.title;
+    }else{
+        eventText.innerText;
+    }
+
+    calendarEvents[month][day - 1] = calenderEvent;
+    localStorage.setItem('events', JSON.stringify(calendarEvents));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    saveButton.addEventListener('click', saveEvent);
 });
 
-cancelButton.addEventListener('click', ()=>{
+//Cancel
+cancelButton.addEventListener('click', ()=> {
     modalcontainer.classList.remove('show');
 });
-}
+
+//Close
+closeButton.addEventListener('click', () => {
+    sidepanelcontainer.classList.remove('show');
+});
+
 
 const months = [
     "January",
